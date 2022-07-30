ec0a3d00acdf6241445730422d406c016f50ce60
Hamedx2 <hamedxteymouri@gmail.com>
2022-07-30 10:54:00 +0430

    final touches!

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render ,get_object_or_404
 from .models import project
 def home(request):
-    projects = project.objects.order_by('-date')[:5]
+    projects = project.objects.order_by('-date')[:3]
     return render(request, 'portfolio.html', { 'projects' : projects })
 
 def detail(request,project_id):
